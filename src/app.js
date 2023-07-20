ea7225725436b2d8cf9bf0582e7653e8e466a0de
성환 <psung9510@gmail.com>
2023-07-20 14:25:55 +0900

    :pencil2: Modify CORS Middleware

--- src/app.js
+++ src/app.js
@@ -1,6 +1,5 @@
 import express from "express";
 import morgan from "morgan";
-import cors from "cors";
 import rootRouter from "./routes/rootRoute";
 import { corsMiddleware } from "./middlewares";
 
@@ -8,10 +7,6 @@ const app = express();
 
 app.set("view engine", "pug");
 app.set("views", process.cwd() + "/src/views");
-
-const corsConfig = {
-  origin: ["http://localhost:3000", "http://52.78.96.229:5000"],
-};
 
 app.use(morgan("dev"));
 app.use(express.json());
